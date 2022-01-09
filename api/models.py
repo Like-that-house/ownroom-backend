7145ddfcc8bb3c5077a9209bc7bdac7b66e42cbd
Seungwoo Kim <sonicdx886@gmail.com>
2022-01-10 02:14:21 +0900

    [feat]: model 추가

--- api/models.py
+++ api/models.py
@@ -30,7 +30,7 @@ class UserManager(BaseUserManager):
             phoneNumber=phoneNumber,
             password=password
         )
-        #user.set_password(password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
@@ -73,3 +73,66 @@ class User(AbstractBaseUser, PermissionsMixin, Base):
     @property
     def is_staff(self):
         return self.is_admin
+
+class Portfolio(Base):
+    class Meta:
+        db_table="portfolio"
+
+    CONCEPT_CHOICES = (
+        ('Modern', '모던'),
+        ('Minimal', '미니멀'),
+        ('Natural', '내추럴'),
+        ('Antique', '엔티크'),
+        ('NorthEurope', '북유럽')
+    )
+
+    CONSULTING_RANGE = (
+        ('R', '가구 추천'),
+        ('P', '가구 추천부터 배치')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='portfolios')
+    title = models.CharField(max_length=100)
+    introduction = models.TextField()
+    numberOfRooms = models.CharField(max_length=1, choices=(('1','원룸'),('2','투룸'),('3','쓰리룸')))
+    floorSpace = models.IntegerField()
+    concept = models.CharField(max_length=11, choices=CONCEPT_CHOICES)
+    consultingRange = models.CharField(max_length=1, choices=CONSULTING_RANGE)
+    description = models.TextField()
+    budget = models.IntegerField()
+    pricePerUnit = models.IntegerField()
+
+    def __str__(self):
+        return 'portfoilo_' + str(self.id)
+
+class Contact(Base):
+    class Meta:
+        db_table="contact"
+
+    isReported = models.BooleanField(default=False)
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    consultant = models.ForeignKey(User, related_name='consultant', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'contact_' + str(self.id) + '_from_' + str(self.user.nickname) + '_to_' + str(self.consultant.nickname)
+
+class File(Base):
+    class Meta:
+        db_table="file"
+
+    isReport = models.BooleanField()
+    url = models.TextField()
+    contact = models.ForeignKey(Contact, related_name='contact', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'file_' + str(self.id)
+
+class Image(Base):
+    class Meta:
+        db_table="image"
+
+    url = models.TextField()
+    portfolio = models.ForeignKey(Portfolio, related_name='portfolio', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'image_' + str(self.id)
