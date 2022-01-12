0be49871255495eac45812f72bee3b66978fd306
yoonjiy <90975718+yoonjiy@users.noreply.github.com>
2022-01-12 16:30:08 +0900

    feat/signup merge (#5)
    * [refactor]: file table field 추가
    
    * [feat]: 장고 어드민 페이지 일부 수정
    
    * [feat]: 회원가입 시 토큰발급
    
    * [feat]: create LoginView
    
    * [fix] 비밀번호 해싱 오류 수정
    
    * [fix] 필요없는 주석 삭제
    
    * [feat]:회원가입 시 전화번호, 비밀번호 validation 추가
    
    Co-authored-by: Seungwoo Kim <sonicdx886@gmail.com>

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from .views import *
 from rest_framework import routers
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('users/signup', RegisterView.as_view()),
+    path('users/login', LoginView.as_view()),
+]
--- api/views.py
+++ api/views.py
@@ -5,13 +5,39 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
-from rest_framework.filters import OrderingFilter
 
 # Create your views here.
 
 User = get_user_model()
 
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (AllowAny,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+#회원가입
+class RegisterView(APIView):
+    permission_classes = (AllowAny,)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            payload = JWT_PAYLOAD_HANDLER(user)
+            jwt_token = JWT_ENCODE_HANDLER(payload)
+            return Response({"token": jwt_token}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
