d1a66d553b982da77c0d8b3f12e330399e5b6289
Seungwoo Kim <sonicdx886@gmail.com>
2022-01-14 00:13:11 +0900

    [refactor]: Contact display 형태 일부 수정

--- api/models.py
+++ api/models.py
@@ -115,7 +115,7 @@ class Contact(Base):
     consultant = models.ForeignKey(User, related_name='owners', on_delete=models.CASCADE)
 
     def __str__(self):
-        return 'contact_' + str(self.id) + '_from_' + str(self.user.nickname) + '_to_' + str(self.consultant.nickname)
+        return 'contact_' + str(self.id) + '_(owner:' + str(self.owner.nickname) + '/consultant:' + str(self.consultant.nickname)+')'
 
 class File(Base):
     class Meta:
