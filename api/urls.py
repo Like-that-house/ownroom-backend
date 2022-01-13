bca5674e6a4025ef83f8ffefe6c67105a63ceafc
Seungwoo Kim <sonicdx886@gmail.com>
2022-01-14 00:13:30 +0900

    [feat]: 컨설팅 신청서,보고서 다운로드 API 개발 완료

--- api/urls.py
+++ api/urls.py
@@ -10,4 +10,7 @@ urlpatterns = router.urls
 urlpatterns += [
     path('users/signup', RegisterView.as_view()),
     path('users/login', LoginView.as_view()),
+    path('consultings/application/download', ConsultingApplicationDownloadView.as_view()),
+    path('consultings/report/download', ConsultingReportDownloadView.as_view()),
+    path('consultings/test', DownloadTest.as_view())
 ]
